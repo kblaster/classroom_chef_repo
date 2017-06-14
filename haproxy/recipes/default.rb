c0b9e7e0e323d66d87f3ada8c5ce34ba495cdce2
pm <pm>
2017-06-14 17:35:12 +0200

    Inicialización del repositorio

---
+++ haproxy/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: haproxy
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
+
+package 'haproxy'
+
+allwebservers = search('node', 'recipes:apache\:\:default')
+
+template '/etc/haproxy/haproxy.cfg' do
+  source 'haproxy.cfg.erb'
+  variables(
+    :webservers => allwebservers
+  )
+ notifies :restart, 'service[haproxy]'
+end
+
+service 'haproxy' do
+  action [:start, :enable]
+end
