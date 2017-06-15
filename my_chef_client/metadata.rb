d4492e3081345f819617a08462c1269daced8b52
kblaster <kblaster@kblaster.kk>
2017-06-15 18:04:37 +0200

    Creado wrapper cookbook my_chef_client para personalizar el comportamiento de chef-client

---
+++ my_chef_client/metadata.rb
@@ -0,0 +1,17 @@
+name 'my_chef_client'
+maintainer 'kblaster'
+maintainer_email 'kblaster@kblaster.kk'
+license 'all_rights'
+description 'Installs/Configures my_chef_client'
+long_description 'Installs/Configures my_chef_client'
+version '0.1.0'
+
+depends 'chef-client'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/my_chef_client/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/my_chef_client' if respond_to?(:source_url)
---
+++ my_chef_client/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: my_chef_client
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
+
+node.default['chef_client']['interval'] = '300'
+
+include_recipe 'chef-client::default'
